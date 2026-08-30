52e4496bb3c6ef3e5e62cd012406842526925586
CRIMEX X HACKER <soumyadey983@gmail.com>
2026-08-30 11:37:57 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Prime XD Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Prime XD Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '919332544935', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/Crimex-X-Hacking/PRIME-XD.zip",
 };
 
 module.exports = settings;
